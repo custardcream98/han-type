c8ea9c5b24d047e98743d6f1cd8e949c5cee1c3b
shiwoo.park <custardcream@kakao.com>
2024-04-28 04:34:38 +0900

    feat: 레이아웃

--- apps/service/src/app/layout.tsx
+++ apps/service/src/app/layout.tsx
@@ -15,7 +15,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <body className={pretendard.className}>{children}</body>
+      <body className={pretendard.className}>
+        <main className='flex min-h-screen flex-col items-center justify-between pb-8'>
+          <div className='flex min-w-96 grow flex-col  px-8 lg:min-w-[1024px]'>
+            <header className='py-4'>
+              <h1 className='text-3xl font-bold'>한타입</h1>
+            </header>
+            <div className='grow'>{children}</div>
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
