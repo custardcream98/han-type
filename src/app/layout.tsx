79200007f2de5af4ca219b60ad04bfc62c1a608f
shiwoo.park <custardcream@kakao.com>
2024-04-28 02:18:00 +0900

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css"
+
+import { pretendard } from "@/assets/fonts"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  description: "한국어 타자 연습",
+  title: "HanType"
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='ko'>
+      <body className={pretendard.className}>{children}</body>
+    </html>
+  )
+}
