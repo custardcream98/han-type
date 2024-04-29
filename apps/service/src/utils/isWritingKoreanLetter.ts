1fc31e3adf8ee190587eba0a86f3e2972804e45d
shiwoo.park <custardcream@kakao.com>
2024-04-30 04:06:08 +0900

    feat: 글자 입력 유효성 검사 유틸 추가

---
+++ apps/service/src/utils/isWritingKoreanLetter.ts
@@ -0,0 +1,50 @@
+import { disassemble } from "hangul-js"
+
+export const isWritingKoreanLetter = ({
+  targetValue,
+  value,
+  nextValue,
+  nextTypedValue
+}: {
+  targetValue: string
+  value: string
+  nextValue?: string
+  nextTypedValue?: string
+}) => {
+  if (value === targetValue) {
+    return true
+  }
+
+  const target자소 = disassemble(targetValue)
+  const 자소 = disassemble(value)
+
+  if (자소.length > target자소.length) {
+    if (nextValue === undefined) {
+      return false
+    }
+
+    const next자소 = disassemble(nextValue)
+
+    if (nextTypedValue) {
+      const nextTyped자소 = disassemble(nextTypedValue)
+
+      return [...자소, ...nextTyped자소].every((letter, index) => {
+        return (
+          letter === target자소[index] ||
+          letter === next자소[index - target자소.length]
+        )
+      })
+    }
+
+    return 자소.every((letter, index) => {
+      return (
+        letter === target자소[index] ||
+        letter === next자소[index - target자소.length]
+      )
+    })
+  }
+
+  return 자소.every((letter, index) => {
+    return letter === target자소[index]
+  })
+}
