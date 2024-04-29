b513b8d6802f5f3cf3fc7b1f7324cd9348c69cde
shiwoo.park <custardcream@kakao.com>
2024-04-29 23:34:30 +0900

    feat: 레이아웃 변경

--- apps/service/src/app/layout.tsx
+++ apps/service/src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
     <html lang='ko'>
       <body className={pretendard.className}>
         <main className='flex min-h-screen flex-col items-center justify-between pb-8'>
-          <div className='flex min-w-96 grow flex-col  px-8 lg:min-w-[1024px]'>
-            <header className='py-4'>
+          <div className='mx-auto flex min-w-96 max-w-4xl grow flex-col px-8'>
+            <header className='pb-12 pt-28'>
               <h1 className='text-3xl font-bold'>한타입</h1>
             </header>
             <div className='grow'>{children}</div>
