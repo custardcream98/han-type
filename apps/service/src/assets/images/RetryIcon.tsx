2adb049defe14678b0aec8a28e0b3d06d1465f40
shiwoo.park <custardcream@kakao.com>
2024-05-04 01:16:49 +0900

    fix: 잘못된 SVG prop 명 수정

--- apps/service/src/assets/images/RetryIcon.tsx
+++ apps/service/src/assets/images/RetryIcon.tsx
@@ -3,9 +3,9 @@ export const RetryIcon = (props: React.SVGAttributes<SVGElement>) => (
     <path
       fill='none'
       stroke='currentColor'
-      stroke-linecap='round'
-      stroke-linejoin='round'
-      stroke-width='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
       d='M10 8H5V3m.291 13.357a8 8 0 1 0 .188-8.991'
     />
   </svg>
